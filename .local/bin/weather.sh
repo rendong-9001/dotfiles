9e5dae3cffebc26e799d14d2fdda0f1a8d6ddb6d
rendong <rendongetz@gmail.com>
2025-11-28 23:38:12 +0800

    chore(.local/bin/weather.sh): add weather.sh

---
+++ .local/bin/weather.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+sleep 5
+FILE_PATH='/tmp/weather'
+CITY='武汉'
+INTERVAL=1800
+
+[ -f "$FILE_PATH" ] || touch "$FILE_PATH"
+while :; do
+	weather=$(curl -s --max-time 3 wttr.in/$CITY?format=%l:+%C+%t\&lang=zh 2>/dev/null)
+	if [ -n "$weather" ]; then
+		echo "$weather\n" > "$FILE_PATH"
+	fi
+	sleep $INTERVAL
+done
