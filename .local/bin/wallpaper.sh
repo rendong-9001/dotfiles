a1737c6dbe22add74991c55084946ee8bc5f6e16
rendong <rendongetz@gmail.com>
2025-12-01 21:52:48 +0800

    chore(.local/bin/): improve weather.sh & wallpaper.sh

--- .local/bin/wallpaper.sh
+++ .local/bin/wallpaper.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
+set -eu
 
 sleep 1 
-WALLPAPER_DIR="$HOME/.local/share/wallpapers"
-WALLPAPER_PROG="$(which swww)"
+WALLPAPER_INTERVAL=${WALLPAPER_INTERVAL:-3600}
+WALLPAPER_DIR=${WALLPAPER_DIR:-"$HOME/.local/share/wallpapers"}
+WALLPAPER_PROG="swww"
+
+trap 'logger -t "wallpaper.sh" -p "user.info" "terminating"; exit 0' INT TERM
+
+if [ ! -d "$WALLPAPER_DIR" ] || ! command -v "$WALLPAPER_PROG" >/dev/null 2>&1; then 
+  	exit 1
+fi
 
 while :; do
-	if ! command -v "$WALLPAPER_PROG" >/dev/null 2>&1; then
-			sleep 1
-			continue
+	if ! pgrep -x 'swww-daemon' >/dev/null 2>&1; then
+		logger -t 'wallpaper.sh' -p 'user.err' 'swww-daemon: not running'
+		sleep 2
+		continue
 	fi
-	wallpaper=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
+	wallpaper=$(find "$WALLPAPER_DIR" -type f -print \( -iname "*.png" -o -iname "*.jpg" \) | shuf -n 1)
 	swww img -t outer --transition-pos top $wallpaper
-	sleep 3600
+	sleep "$WALLPAPER_INTERVAL"
 done
