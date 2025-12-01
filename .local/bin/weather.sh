fa865ab8786982787ccc469639783840052e5dd9
rendong <rendongetz@gmail.com>
2025-12-01 21:52:48 +0800

    chore(.local/bin/): improve weather.sh & wallpaper.sh

--- .local/bin/weather.sh
+++ .local/bin/weather.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
+set -eu
 
-sleep 5
+sleep 4
 FILE_PATH='/tmp/weather'
-CITY='武汉'
-INTERVAL=1800
+CITY=${CITY:-'武汉'}
+WEATHER_INTERVAL=${WEATHER_INTERVAL:-1800}
+
+trap 'logger -t "weather.sh" -p "user.info" "terminating"; exit 0' INT TERM
 
 [ -f "$FILE_PATH" ] || :>"$FILE_PATH"
+
 while :; do
+	if ! command -v curl >/dev/null 2>&1; then
+		logger -t 'weather.sh' -p 'user.err' 'curl: not found'
+		sleep 2
+		continue
+	fi
 	weather=$(curl -s --max-time 3 wttr.in/$CITY?format=%l:+%C+%t\&lang=zh 2>/dev/null)
 	if [ -n "$weather" ]; then
 		echo "$weather" > "$FILE_PATH"
 	fi
-	sleep $INTERVAL
+	sleep $WEATHER_INTERVAL
 done
