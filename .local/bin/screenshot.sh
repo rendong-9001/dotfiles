51461c05b3af6bbb398bdd54935b93be995e4a24
rendong <rendongetz@gmail.com>
2025-12-28 22:07:43 +0800

    chore(.local/bin/{screencast.sh,screenshot.sh}): fixed and improved

--- .local/bin/screenshot.sh
+++ .local/bin/screenshot.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
+set -eu
 
 for cmd in grim slurp; do
 	command -v "$cmd" >/dev/null 2>&1 || { notify-send "$cmd not installed"; exit 1; }
 done
 
-MODE="${1:-region}"
-
-SCREENSHOTS_PATH="$XDG_PICTURES_DIR/Screenshots"
-[ -d "$SCREENSHOTS_PATH" ] || mkdir -p "$SCREENSHOTS_PATH"
+timestamp() { date +%F-%H-%M-%S; }
 
 check_esc() {
-	if [ -z "$1"]; then
+	if [ -z "$1" ]; then
 		notify-send "Screenshot" "Capture cancelled"
 		exit 0
 	fi
 }
 
-FILE="$SCREENSHOTS_PATH/$(date +%F-%H-%M-%S).png"
+MODE="${1:-region}"
+SCREENSHOTS_PATH="$XDG_PICTURES_DIR/Screenshots"
+[ -d "$SCREENSHOTS_PATH" ] || mkdir -p "$SCREENSHOTS_PATH"
+FILE="$SCREENSHOTS_PATH/$(timestamp).png"
 
 case "$MODE" in
 	full)
