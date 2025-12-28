947487ae494ecc668fd0d611f2f06311e3791188
rendong <rendongetz@gmail.com>
2025-12-28 22:07:43 +0800

    chore(.local/bin/{screencast.sh,screenshot.sh}): fixed and improved

--- .local/bin/screencast.sh
+++ .local/bin/screencast.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
+set -eu
 
-for cmd in fuzzel wf-recorder; do
+for cmd in fuzzel wf-recorder wlr-randr; do
     if ! command -v "$cmd" >/dev/null 2>&1; then
         notify-send "$cmd not installed"
         exit 1
     fi
 done
 
-SCREENCAP_PATH="$XDG_VIDEOS_DIR/Screencasts"
-[ -d "$SCREENCAST_PATH" ] || mkdir -p "$SCREENCAP_PATH"
-FILE="$SCREENCAST_PATH/$(date +%F-%H-%M-%S).mp4"
+timestamp() { date +%F-%H-%M-%S; }
+
+SCREENCASTS_PATH="$XDG_VIDEOS_DIR/Screencasts"
+[ -d "$SCREENCASTS_PATH" ] || mkdir -p "$SCREENCAPS_PATH"
+FILE="$SCREENCASTS_PATH/$(timestamp).mp4"
 
 case "$1" in
     start)
-        _output=$(wlr-randr | awk -F '[" ]' '/^[^ ]/ {print $1}' | fuzzel -d -p "Select Output:")
-        _fps=$(echo "45\n60\n120" | fuzzel -d -p "Select FPS:")
-        echo $_fps > /tmp/test
+        _output=$(wlr-randr | awk '!/^[[:space:]]/ { print $1 }' | fuzzel -d -p "Select Output:")
+        _fps=$(printf '%s\n' 45 60 120 | fuzzel -d -p "Select FPS:")
         wf-recorder -r "${_fps:-60}" --audio -f "$FILE" -o "$_output" >/dev/null 2>&1 &
         notify-send "Screencast" "Recording started"
         ;;
     stop)
-        pkill wf-recorder >/dev/null 2>&1
+        pkill wf-recorder >/dev/null 2>&1 || true
         notify-send "Screencast" "Recording finished"
         ;;
     *)
