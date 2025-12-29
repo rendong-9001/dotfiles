d913f0ebbde57ae3b131f73a0f44935844b14654
rendong <rendongetz@gmail.com>
2025-12-29 20:11:26 +0800

    chore(.local/bin/{weather.sh,wallpaper.sh}): rm "trap"

--- .local/bin/wallpaper.sh
+++ .local/bin/wallpaper.sh
@@ -6,8 +6,6 @@ WALLPAPER_INTERVAL=${WALLPAPER_INTERVAL:-3600}
 WALLPAPER_DIR=${WALLPAPER_DIR:-"$HOME/.local/share/wallpapers"}
 WALLPAPER_PROG="swww"
 
-trap 'logger -t "wallpaper.sh" -p "user.info" "terminating"; exit 0' INT TERM
-
 if [ ! -d "$WALLPAPER_DIR" ] || ! command -v "$WALLPAPER_PROG" >/dev/null 2>&1; then 
   	exit 1
 fi
@@ -15,7 +13,7 @@ fi
 while :; do
 	if ! pgrep -x 'swww-daemon' >/dev/null 2>&1; then
 		logger -t 'wallpaper.sh' -p 'user.err' 'swww-daemon: not running'
-		sleep 2
+		sleep 1
 		continue
 	fi
 	wallpaper=$(find "$WALLPAPER_DIR" -type f -print \( -iname "*.png" -o -iname "*.jpg" \) | shuf -n 1)
