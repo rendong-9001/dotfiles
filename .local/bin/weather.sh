ee9cef78cef936149043aa2fc1e4ea48d8e6150e
rendong <rendongetz@gmail.com>
2025-12-29 20:11:26 +0800

    chore(.local/bin/{weather.sh,wallpaper.sh}): rm "trap"

--- .local/bin/weather.sh
+++ .local/bin/weather.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
-sleep 3
+sleep 1
 FILE_PATH='/tmp/weather'
 CITY=${CITY:-'武汉'}
 WEATHER_INTERVAL=${WEATHER_INTERVAL:-1800}
-
-trap 'logger -t "weather.sh" -p "user.info" "terminating"; exit 0' INT TERM
 
 [ -f "$FILE_PATH" ] || :>"$FILE_PATH"
 
 while :; do
 	if ! command -v curl >/dev/null 2>&1; then
 		logger -t 'weather.sh' -p 'user.err' 'curl: not found'
-		sleep 2
+		sleep 1
 		continue
 	fi
 	weather=$(curl -s --max-time 3 wttr.in/$CITY?format=%l:+%C+%t\&lang=zh 2>/dev/null)
