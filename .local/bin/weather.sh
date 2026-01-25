d7e917a569468c94a2f8625fec6c4c2eb1cfe24d
rendong <rendongetz@gmail.com>
2026-01-25 20:55:21 +0800

    refactor(.local/bin/weather.sh): use flock to prevent duplicate startup

--- .local/bin/weather.sh
+++ .local/bin/weather.sh
@@ -1,21 +1,47 @@
 #!/bin/sh
+set -eu
 
-sleep 1
-FILE_PATH='/tmp/weather'
-CITY=${CITY:-'武汉'}
-WEATHER_INTERVAL=${WEATHER_INTERVAL:-1800}
+CITY=武汉
+WEATHER_INTERVAL=30m
+DATA_DIR="/tmp/weather"
+LOCK="$DATA_DIR/lock"
+PID="$DATA_DIR/pid"
+RESULT="$DATA_DIR/result"
 
-[ -f "$FILE_PATH" ] || :>"$FILE_PATH"
+[ -d "$DATA_DIR" ] || mkdir -p "$DATA_DIR"
+{
+	exec 3>"$LOCK"
+	flock -n 3 || exit 1
+}
+exec >"$DATA_DIR/log" 2>&1
+timestamp() { date +%F-%H-%M-%S; }
+log() {
+	printf "%s [%s]: %s\n" "$(timestamp)" "$1" "$2"
+}
+if ! command -v curl >/dev/null 2>&1; then
+	log 'error' 'curl not found'
+	exit 1
+fi
+cleanup() {
+	log 'info' 'stopping'
+	if [ -n "${sleep_pid:-}" ]; then
+		kill "$sleep_pid" 2>/dev/null || :
+	fi
+	:> "$PID"
+}
+trap '{ cleanup; exit 0; }' TERM INT EXIT
+printf "%s\n" "$$" >"$PID"
 
 while :; do
-	if ! command -v curl >/dev/null 2>&1; then
-		logger -t 'weather.sh' -p 'user.err' 'curl: not found'
-		sleep 1
+	URL="https://wttr.in/$CITY?format=%l:+%C+%t\&lang=zh"
+	weather=$(curl -s --max-time 3 "$URL") || :
+	if [ -z "$weather" ]; then
+		sleep 5
 		continue
 	fi
-	weather=$(curl -s --max-time 3 wttr.in/$CITY?format=%l:+%C+%t\&lang=zh 2>/dev/null)
-	if [ -n "$weather" ]; then
-		echo "$weather" > "$FILE_PATH"
-	fi
-	sleep $WEATHER_INTERVAL
+	tmp_result="$(mktemp "$DATA_DIR/result.XXXXXX")" || exit 1
+	printf "%s\n" "$weather" >"$tmp_result" && mv "$tmp_result" "$RESULT"
+	sleep "${WEATHER_INTERVAL:-1800}" &
+	sleep_pid="$!"
+	wait "$sleep_pid" || :
 done
