38c7f3f0743c1f3dcc30d764b9e283d282459cc0
rendong <rendongetz@gmail.com>
2026-03-05 18:17:17 +0800

    chore(.local/bin/wallpaper.sh): same as 'weather.sh'

--- .local/bin/wallpaper.sh
+++ .local/bin/wallpaper.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 set -eu
-
-sleep 1 
+ 
 WALLPAPER_INTERVAL=${WALLPAPER_INTERVAL:-3600}
 WALLPAPER_DIR=${WALLPAPER_DIR:-"$HOME/.local/share/wallpapers"}
 WALLPAPER_PROG="swww"
+DATA_DIR='/tmp/wallpaper'
+LOCK="$DATA_DIR/lock"
+PID="$DATA_DIR/pid"
 
-if [ ! -d "$WALLPAPER_DIR" ] || ! command -v "$WALLPAPER_PROG" >/dev/null 2>&1; then 
+[ -d "$DATA_DIR" ] || mkdir -p "$DATA_DIR"
+{
+	exec 4>"$LOCK"
+	flock -n 4 || exit 1
+}
+exec >"$DATA_DIR/log" 2>&1
+timestamp() { date +%F-%H-%M-%S; }
+log() {
+	printf "%s [%s]: %s\n" "$(timestamp)" "$1" "$2"
+}
+
+if [ ! -d "$WALLPAPER_DIR" ] || ! command -v "$WALLPAPER_PROG" >/dev/null 2>&1; then
   exit 1
 fi
 
@@ -14,14 +27,16 @@ interrupt_sleep() {
 	if [ -n "${_sleep_pid:-}" ]; then
 		kill "$_sleep_pid" 2>/dev/null || :
 	fi
+	:> "$PID"
 }
 
-trap 'interrupt_sleep' USR1
-trap '{ interrupt_sleep; exit 0; }' INT TERM EXIT
+trap 'exit 0' INT TERM
+trap 'interrupt_sleep' USR1 EXIT
+printf "%s\n" "$$" >"$PID"
 
 while :; do
 	if ! pgrep -x 'swww-daemon' >/dev/null 2>&1; then
-		logger -t 'wallpaper.sh' -p 'user.err' 'swww-daemon: not running'
+		log 'error' 'swww-daemon: not running'
 		sleep 1
 		continue
 	fi
@@ -29,5 +44,7 @@ while :; do
 	swww img -t outer --transition-pos top $wallpaper
 	sleep "$WALLPAPER_INTERVAL" &
 	_sleep_pid="$!"
-	wait "$_sleep_pid" || :
+	wait "$_sleep_pid" || {
+		log 'info' "killed $_sleep_pid"
+	}
 done
