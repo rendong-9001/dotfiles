cd8a9b5de0266674328ac9de5379cdeecd854956
rendong <rendongetz@gmail.com>
2026-05-12 06:21:14 +0800

    chore(env-update.sh): add XDG_SESSION_TYPE

--- .local/bin/env-update.sh
+++ .local/bin/env-update.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 set -eu
 
-UPDATE_ENV_PROG='dbus-update-activation-environment'
+vars="
+ PATH
+ DISPLAY
+ WAYLAND_DISPLAY
+ XDG_DATA_HOME
+ XDG_STATE_HOME
+ XDG_CACHE_HOME
+ XDG_CONFIG_HOME
+ XDG_SESSION_TYPE
+ XDG_CURRENT_DESKTOP
+"
 
-if command -v turnstile-update-runit-env >/dev/null 2>&1 ; then
-    UPDATE_ENV_PROG='turnstile-update-runit-env'
+if command -v turnstile-update-runit-env >/dev/null 2>&1; then
+	turnstile-update-runit-env $vars
 fi
 
-"$UPDATE_ENV_PROG" \
-    DISPLAY \
-    WAYLAND_DISPLAY \
-    XDG_RUNTIME_DIR \
-    XDG_CURRENT_DESKTOP
+if command -v dbus-update-activation-environment >/dev/null 2>&1; then
+	dbus-update-activation-environment $vars
+fi
