fdd863270e5415e2c25e294be202406506758def
rendong <rendongetz@gmail.com>
2026-05-12 05:25:23 +0800

    feat(startwm.sh): add -l flag to sh & create user dirs & source .profile

--- .local/bin/startwm.sh
+++ .local/bin/startwm.sh
@@ -1,30 +1,57 @@
 #!/bin/sh
 set -eu
 
-: "${XDG_CURRENT_DESKTOP:=}"
+: "${XDG_SESSION_TYPE:=wayland}"
+: "${XDG_CURRENT_DESKTOP:=river}"
 
-start_wm() {
-	if command -v turnstiled >/dev/null 2>&1; then
-		exec "$@"
+: "${XDG_RUNTIME_DIR:=/run/user/$(id -u)}"
+: "${DBUS_SESSION_BUS_ADDRESS:=unix:path=$XDG_RUNTIME_DIR/bus}"
+
+set_env() {
+	if [ -f "$HOME/.profile" ]; then
+		. "$HOME/.profile"
 	fi
-	exec dbus-run-session "$@"
+	export XDG_SESSION_TYPE XDG_CURRENT_DESKTOP
 }
 
-start_shell(){
+mk_dirs() {
+	mkdir -p \
+		"$HOME/.local/bin" \
+		"${XDG_CACHE_HOME:-$HOME/.cache}" \
+		"${XDG_CONFIG_HOME:-$HOME/.config}" \
+		"${XDG_DATA_HOME:-$HOME/.local/share}" \
+		"${XDG_STATE_HOME:-$HOME/.local/state}"
+}
+
+start_wm() {
+	set_env
+	mk_dirs
+	if dbus-send --bus="$DBUS_SESSION_BUS_ADDRESS" / org.freedesktop.DBus.Peer.Ping >/dev/null 2>&1; then
+		exec "$@"
+	fi
+	exec dbus-run-session -- "$@"
+}
+
+start_shell() {
+	mk_dirs
 	if command -v bash >/dev/null 2>&1; then
 		exec bash -l
 	fi
-	exec sh
+	exec sh -l
 }
 
-case "$XDG_CURRENT_DESKTOP" in
-	river)
-		start_wm river
-		;;
-	niri)
-		start_wm niri
-		;;
-	*)
-		start_shell
-		;;
-esac
+if [ "$XDG_SESSION_TYPE" = wayland ]; then
+	case "$XDG_CURRENT_DESKTOP" in
+		river)
+			start_wm river
+			;;
+		niri)
+			start_wm niri --session
+			;;
+		*)
+			start_shell
+			;;
+	esac
+else
+	start_shell
+fi
