8daa5ffad2fbf908bb1fafb51c44a0c6cd6fd63a
rendong <rendongetz@gmail.com>
2026-05-19 17:15:32 +0800

    refactor(wallpaper.sh): use xdg env vars

--- .local/bin/wallpaper-update.sh
+++ .local/bin/wallpaper-update.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 set -eu
 
+: "${XDG_RUNTIME_DIR:=/tmp}"
+
 UID=$(id -u)
-PID_FILE="/tmp/wallpaper-$UID/pid"
-kill -USR1 $(cat < $PID_FILE) || :
+DATA_DIR='wallpaper'
+PID_FILE="$XDG_RUNTIME_DIR/$DATA_DIR-$UID/pid"
+PID=$(cat "$PID_FILE")
+
+if [ -z "$PID" ]; then
+	notify-send "${0##*/}" "Daemon may not running" || :
+	exit 1
+fi
+
+kill -USR1 "$PID" 2>/dev/null || :
