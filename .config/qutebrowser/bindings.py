33e14307edcac9cafd55993fc05f031fdcc6d470
rendong <rendongetz@gmail.com>
2026-05-28 17:27:08 +0800

    feat(qutebrowser): init commit

---
+++ .config/qutebrowser/bindings.py
@@ -0,0 +1,3 @@
+config.bind('<Alt+Left>', 'back', mode='normal')
+config.bind('<Alt+Right>', 'forward', mode='normal')
+config.bind('<Control+R>', 'reload', mode='normal')
---
+++ .config/qutebrowser/theme.py
@@ -0,0 +1,50 @@
+from colors import *
+
+c.colors.webpage.darkmode.enabled = True
+
+c.colors.statusbar.normal.bg = COLOR_BLACK_N
+c.colors.statusbar.normal.fg = COLOR_WHITE_N
+
+c.colors.statusbar.command.bg = COLOR_BLACK_N
+c.colors.statusbar.command.fg = COLOR_WHITE_B
+
+c.colors.statusbar.insert.bg = COLOR_GREEN_N
+c.colors.statusbar.insert.fg = COLOR_BLACK_N
+
+c.colors.statusbar.url.success.http.fg = COLOR_GREEN_B
+c.colors.statusbar.url.hover.fg = COLOR_CYAN_B
+c.colors.statusbar.progress.bg = COLOR_BLUE_N
+
+c.colors.tabs.even.bg = COLOR_BLACK_N
+c.colors.tabs.even.fg = COLOR_BLUE_B
+c.colors.tabs.odd.bg = COLOR_BLACK_N
+c.colors.tabs.odd.fg = COLOR_BLUE_B
+
+c.colors.tabs.selected.even.bg = COLOR_GREEN_N
+c.colors.tabs.selected.even.fg = COLOR_BLACK_N
+c.colors.tabs.selected.odd.bg = COLOR_GREEN_N
+c.colors.tabs.selected.odd.fg = COLOR_BLACK_N
+
+c.colors.hints.bg = COLOR_YELLOW_B
+c.colors.hints.fg = COLOR_BLACK_N
+c.colors.hints.match.fg = COLOR_RED_N
+
+c.colors.completion.fg = COLOR_WHITE_N
+
+c.colors.completion.category.bg = COLOR_BLACK_N
+c.colors.completion.category.fg = COLOR_YELLOW_N
+c.colors.completion.category.border.bottom = COLOR_BLACK_N
+
+c.colors.completion.item.selected.bg = COLOR_BLUE_N
+c.colors.completion.item.selected.fg = COLOR_BLACK_N
+c.colors.completion.item.selected.border.top = COLOR_BLUE_N
+c.colors.completion.item.selected.border.bottom = COLOR_BLUE_N
+
+c.colors.completion.match.fg = COLOR_MAGENTA_B
+
+c.colors.messages.info.bg = COLOR_BLACK_N
+c.colors.messages.info.fg = COLOR_CYAN_B
+c.colors.messages.error.bg = COLOR_RED_N
+c.colors.messages.error.fg = COLOR_WHITE_B
+c.colors.messages.warning.bg = COLOR_YELLOW_N
+c.colors.messages.warning.fg = COLOR_BLACK_N
